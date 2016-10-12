0c642ca71c4cdd744f0af40b81987833fb8a2314
sdpernick <sarah.pernick@gmail.com>
2016-10-11 22:57:55 -0400

    Classwork

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Tip: You can either hide the answer on page load with js or use display:none in css
+
+$('#question1').on('click',function(){
+   $('#answer1').slideToggle();
+});
+
+$('#question2').on('click',function(){
+   $('#answer2').slideToggle();
+});
+
+$('#question3').on('click',function(){
+   $('#answer3').slideToggle();
+});
